b473802fb361834cc33e151013399e464a0f84c3
Константин Дьяченко <konard@me.com>
2020-02-09 20:06:17 +0300

    4th task's solution draft finilized.

--- cpp/T4_1/T4_1.cpp
+++ cpp/T4_1/T4_1.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 using namespace std;
 
+#define EPSILON (1.0E-8)
+
 double logarithm(double a, double x)
 {
     return log(x) / log(a);
@@ -10,6 +12,11 @@ double logarithm(double a, double x)
 double F(double b, double c, double x)
 {
     return logarithm(5.0 / 11.0, x * x - c) / sqrt(b * tan(1.0 / x * x) - pow(sin(abs(x * sqrt(x))), x / 2));
+}
+
+bool checkX(double b, double x)
+{
+    return abs(b * tan(1.0 / x * x) - pow(sin(abs(x * sqrt(x))), x / 2)) > EPSILON;
 }
 
 int main()
@@ -22,9 +29,29 @@ int main()
 
     cout << "Enter x: ";
     cin >> x;
+    if (x <= EPSILON)
+    {
+        cout << "x should be greater than 0." << endl;
+        return 1;
+    }
+    if (!checkX(b, x))
+    {
+        cout << "b * tan(1.0 / x * x) - pow(sin(abs(x * sqrt(x))), x / 2) expression should't be close to 0." << endl;
+        return 1;
+    }
 
     cout << "Enter a: ";
     cin >> a;
+    if (a <= EPSILON)
+    {
+        cout << "a should be greater than 0." << endl;
+        return 1;
+    }
+    if (abs(a - 1) <= EPSILON)
+    {
+        cout << "a should't be equal to 1." << endl;
+        return 1;
+    }
 
     cout << "F1(b, c, x) = " << F(b, c, x) << endl;
     cout << "F2(a, x) = " << logarithm(a, x) << endl;
